2ee5daf8d4da6fce6c32eb4944e616255effbc96
amulu <amulu@LAPTOP-RVC5B139>
2021-07-07 22:04:27 +0530

    initial commit

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -2,7 +2,19 @@ public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
 
 		// WRITE YOUR LOGIC HERE...
-		System.out.println(square);
-		return true;
+		//if the row number is even, black squares at odd places
+		if((square/8)%2==0) {
+			if(square%2!=0) {
+				return true;
+			}
+		}
+		//if row number is odd, black squares at even places
+		else {
+			if(square%2==0) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
+
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -3,15 +3,11 @@ public class GameStrategy {
 	private int numQueens = 0;
 
 	private int getColumn(int cellId) {
-		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+		return cellId%8;
 	}
 	
 	private int getRow(int cellId) {
-		// WRITE YOUR LOGIC HERE....................................
-		
-		return 0;	// just for the heck of it
+		return cellId/8;
 	}
 
 	public boolean isValidPosition(int cellId) {
@@ -24,14 +20,69 @@ public class GameStrategy {
 		int col = getColumn(cellId);
 		
 		int row = getRow(cellId);
+		//check if any queen is present in the row or col, if present, invalid move
+		for(int i=0;i<8;i++) {
+			if(placedQueens[row][i]==true || placedQueens[i][col]==true) {
+				return false;
+			}
+		}
 		
-		/*
-			WRITE YOUR LOGIC HERE...............................
-
-		*/
+		//check for diagonal queens
+		int i = row;
+		int j = col;
+		//upper negative diagonal
+		while(i>=0 && j>=0) {
+			if(placedQueens[i--][j--]==true) {
+				return false;
+			}
+		}
+		i=row;
+		j=col;
+		//lower negative diagonal
+		while(i<=7 && j<=7) {
+			if(placedQueens[i++][j++]==true) {
+				return false;
+			}
+		}
+		i=row;
+		j=col;
+		//upper positive diagonal
+		while(i>=0 && j<=7) {
+			if(placedQueens[i--][j++]==true) {
+				return false;
+			}
+		}
+		i=row;
+		j=col;
+		//lower positive diagonal
+		while(i<=7 && j>=0) {
+			if(placedQueens[i++][j--]==true) {
+				return false;
+			}
+		}
+		//
+		placedQueens[row][col] = true;
+		numQueens++;
+		System.out.println("valid");
 		return isValid;
 	}
 }
+
+/**
+ *  0   	1		2		3		4		5		6		7           0
+ *  8		9		10		11		12		13		14		15			1
+ *  16		17		18		19		20		21		22		23			2
+ *  24		25		26		27		28		29		30		31			3
+ *  32		33		34		35		36		37		38		39			4
+ *  40		41		42		43		44		45		46		47			5
+ *  48		49		50		51		52		53		54		55			6
+ *  56		57		58		59		60		61		62		63			7
+ *  
+ *  
+ *  arr[2][4]
+ *  
+ *  
+ */
 
 
 
